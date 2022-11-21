0a9a9303567cf434057268813ad308c8b18bce91
Parija <parija.jamatia.pj@gmail.com>
2022-11-21 17:20:19 +0530

    second commit

--- main.js
+++ main.js
@@ -1,14 +1,16 @@
 var form = document.getElementById('addForm');
 var itemList = document.getElementById('items');
-//var filter = document.getElementById('filter');
+var filter = document.getElementById('filter');
+
 
 // Form submit event
 form.addEventListener('submit', addItem);
+
 // Delete event
 itemList.addEventListener('click', removeItem);
 
-//Edit event
-itemList.addEventListener('click', editItem);
+//Filter Events
+filter.addEventListener('keyup', filterItems);
 
 // Add item
 function addItem(e){
@@ -16,13 +18,14 @@ function addItem(e){
 
   // Get input value
   var newItem = document.getElementById('item').value;
+  var newItemDescription = document.getElementById('item-description').value;
 
   // Create new li element
   var li = document.createElement('li');
   // Add class
   li.className = 'list-group-item';
   // Add text node with input value
-  li.appendChild(document.createTextNode(newItem));
+  li.appendChild(document.createTextNode(newItem + " " +newItemDescription));
 
   // Create del button element
   var deleteBtn = document.createElement('button');
@@ -53,4 +56,26 @@ function removeItem(e){
       itemList.removeChild(li);
     }
   }
+}
+
+// Filter Items
+function filterItems(e){
+  // convert text to lowercase
+  var text = e.target.value.toLowerCase();
+  // Get lis
+  var items = itemList.getElementsByTagName('li');
+  // Convert to an array
+  Array.from(items).forEach(function(item){
+    var itemName1 = item.firstChild.textContent;
+    var itemName2 = item.childNodes[1].textContent;
+
+    if(itemName1.toLowerCase().indexOf(text) != -1){
+      item.style.display = 'block';
+    }
+    else if(itemName2.toLowerCase().indexOf(text) != -1){
+      item.style.display = 'block';
+    } else {
+      item.style.display = 'none';
+    }
+  });
 }
